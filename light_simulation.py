81a3c1de42a4750ac5ef9325b1fab44b28d95416
ashraftuyubahe <ashrafutyubahe001u@gmail.com>
2025-03-14 18:08:54 +0200

    making documentation

--- light_simulation.py
+++ light_simulation.py
@@ -1,11 +1,11 @@
 import paho.mqtt.client as mqtt
 
-# MQTT broker details
+
 broker = "157.173.101.159"
 port = 1883
 topic = "/student_group/light_control"
 
-# Define message handler
+
 def on_message(client, userdata, message):
     msg = message.payload.decode()
     if msg == "ON":
@@ -15,17 +15,17 @@ def on_message(client, userdata, message):
     else:
         print(f"❌ Unknown message received: {msg}")
 
-# Setup MQTT client
+
 client = mqtt.Client()
 client.on_message = on_message
 
-# Connect to broker
+
 client.connect(broker, port, 60)
 
-# Subscribe to topic
+
 client.subscribe(topic)
 
 print(f"🚀 Listening for MQTT messages on {topic}...")
 
-# Keep listening
+
 client.loop_forever()
